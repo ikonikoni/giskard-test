b9b130846797975a35f5b0aeab20d4d1ab0922a6
Inoki <veyx.shaw@outlook.com>
2023-03-23 15:40:16 +0100

    Add falcon status info loading before running backend

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,18 @@
 from flask import Flask, render_template, request, jsonify
 
 import os
+import sys
+import json
 
 from millennium_falcon import odds
+
+# Load millennium falcon status
+def print_help():
+    print("Usage:                                                           ")
+    print("    ./app.py <millennium-falcon.json>")
+
+global falcon_status
+global planets
 
 app = Flask(__name__, template_folder=os.path.abspath('.'))
 
@@ -28,4 +38,24 @@ def empire_plan_uploader():
 
 # run the application
 if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print_help()
+        exit(1)
+
+    try:
+        with open(sys.argv[1], "r") as falcon_status_fd:
+            # Falcon auto-check
+            falcon_status = json.load(falcon_status_fd)
+            if not odds.falcon_autocheck(falcon_status):
+                print("Falcon auto-check failed")
+                exit(1)
+            
+            db_path = os.path.join(os.path.dirname(sys.argv[1]),\
+                falcon_status[odds.ROUTE_DB_KEY])
+            # Falcon data load
+            planets = odds.retrieve_falcon_db_data(db_path)
+    except Exception as e:
+        print("Exception:", e)
+        exit(1)
+
     app.run(debug=True)
